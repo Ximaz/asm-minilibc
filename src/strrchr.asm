0213da93e2c6224ecd2f21cdef67a7b0c2bbe1dc
DURAND Malo <malo.durand@epitech.eu>
2024-02-07 13:19:58 +0100

    adding the strtchr function

---
+++ src/strrchr.asm
@@ -0,0 +1,35 @@
+BITS 64
+section .text
+global strchr
+
+strlen:
+    xor rax, rax
+
+    .while:
+    cmp byte [rdi*1+rax], 0
+    jnz .continue
+
+    ret
+
+    .continue:
+    inc rax
+    jmp .while
+
+strrchr:
+    call strlen
+    sub rax
+
+    .while:
+    cmp byte [rdi*1+rax], 0
+    jz .return
+
+    cmp byte [rdi*1+rax], sil
+    jnz .continue
+
+    .return:
+    lea rax, [rdi*1+rax]
+    ret
+
+    .continue:
+    dec rax
+    jmp .while
