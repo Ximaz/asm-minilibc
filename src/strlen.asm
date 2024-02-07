23aad91e1efc2153ea4049f5636ef85e1a30ca77
DURAND Malo <malo.durand@epitech.eu>
2024-02-07 13:11:20 +0100

    adding the strlen function

---
+++ src/strlen.asm
@@ -0,0 +1,16 @@
+BITS 64
+section .text
+global strlen
+
+strlen:
+    xor rax, rax
+
+    .while:
+    cmp byte [rdi*1+rax], 0
+    jnz .continue
+
+    ret
+
+    .continue:
+    inc rax
+    jmp .while
