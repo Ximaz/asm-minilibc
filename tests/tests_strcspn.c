b8d2f0ec33506827d099a9d1f93ee690f5dfb4f9
DURAND Malo <malo.durand@epitech.eu>
2024-02-08 12:54:03 +0100

    fixing strcsnp => strcspn (typo) and adding unit tests for this function.

---
+++ tests/tests_strcspn.c
@@ -0,0 +1,37 @@
+#include "tests.h"
+
+
+Test(_strcspn, compare_with_libc) {
+    const char* str1 = "hello world";
+    const char* reject1 = "aeiou";
+
+    size_t result_custom = _strcspn(str1, reject1);
+    size_t result_libc = strcspn(str1, reject1);
+
+    cr_assert_eq(result_custom, result_libc, "Custom strcspn result does not match libc strcspn.");
+
+    const char* str2 = "12345";
+    const char* reject2 = "aeiou";
+
+    result_custom = _strcspn(str2, reject2);
+    result_libc = strcspn(str2, reject2);
+
+    printf("%ld %ld\n", result_custom, result_libc);
+    cr_assert_eq(result_custom, result_libc, "Custom strcspn result does not match libc strcspn.");
+
+    const char* str3 = "hello world";
+    const char* reject3 = "";
+
+    result_custom = _strcspn(str3, reject3);
+    result_libc = strcspn(str3, reject3);
+
+    cr_assert_eq(result_custom, result_libc, "Custom strcspn result does not match libc strcspn.");
+
+    const char* str4 = "";
+    const char* reject4 = "aeiou";
+
+    result_custom = _strcspn(str4, reject4);
+    result_libc = strcspn(str4, reject4);
+
+    cr_assert_eq(result_custom, result_libc, "Custom strcspn result does not match libc strcspn.");
+}
