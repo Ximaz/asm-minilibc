b35a05e69afbebd558ca1e23214ebb020809d077
DURAND Malo <malo.durand@epitech.eu>
2024-02-14 19:21:18 +0100

    adding bonus file

---
+++ tests/tests_ffs.c
@@ -0,0 +1,14 @@
+#include "tests.h"
+
+Test(ffs, basic_test) {
+    int value = 0b1000000;
+    int expected_result = 7;
+    
+    // Test your _ffs implementation
+    int result = _ffs(value);
+    cr_assert_eq(result, expected_result, "Your _ffs implementation failed for value: %d: %d", value, result);
+    
+    // Test libc ffs implementation
+    result = ffs(value);
+    cr_assert_eq(result, expected_result, "Libc ffs failed for value: %d", value);
+}
